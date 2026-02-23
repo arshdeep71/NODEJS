36e6b47f82560d59a62730b1c82269d5f7d3446e
arshdeep71 <darkwebhost01@gmail.com>
2026-02-23 10:48:52 +0530

    updated

---
+++ controllers/productController.js
@@ -0,0 +1,7 @@
+export function getProduct(req, res) {
+  res.send("This is get request of productRouter");
+}
+
+export function postProduct(req, res) {
+  res.send("This is post request of productRouter");
+}
---
+++ controllers/userController.js
@@ -0,0 +1,7 @@
+export function getUser(req, res) {
+  res.send("This is get request of userRouter");
+}
+
+export function postUser(req, res) {
+  res.send("This is post request of userRouter");
+}
